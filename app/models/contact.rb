1cd627eadcafb4de881e9673b36a0e2d9baabbb7
Pery Lemke <pery.lemke@gmail.com>
2017-03-15 19:04:33 -0300

    Improve the models and the form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :user
-
+  has_many :fields
   validates_presence_of :email
-  
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,6 +1,5 @@
 class Field < ApplicationRecord
   belongs_to :user
-  has_many :contact
   validates :field_name, :field_type, presence: true
   enum field_type: [:text, :textarea, :combobox]
 end
