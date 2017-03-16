5655e6b41178d70c07fdb03666aeebf3d148ac7c
Pery Lemke <pery.lemke@gmail.com>
2017-03-15 21:30:31 -0300

    Improving Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,14 @@
 class Contact < ApplicationRecord
   belongs_to :user
   has_many :fields
+
+  validates_presence_of :name
+  NAME_MIN_LENGTH = 3   #characters
+  NAME_MAX_LENGTH = 50  #characters
+  validates :name, length: {in: NAME_MIN_LENGTH..NAME_MAX_LENGTH}
+
   validates_presence_of :email
-  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: {with: email_regex}
+
 end
